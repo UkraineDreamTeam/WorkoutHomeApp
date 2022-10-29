82663356292f1f1e6492f7f47b69dc3a9c9cad1a
LyubovShuliak <87687676+LyubovShuliak@users.noreply.github.com>
2022-10-29 22:38:33 +0300

    Develop (#5)
    * add different build variants for android
    
    * add ci/cd
    
    * prepare ci cd (#2)
    
    prepare CI/CD for react native (android distribution)
    
    * eslint + prettier
    
    * linter
    
    Co-authored-by: nizkiyVorobey <formobileprima@gmail.com>
    Co-authored-by: Oleksandr Volkov <43185533+nizkiyVorobey@users.noreply.github.com>

--- src/screens/HomeStack.tsx
+++ src/screens/HomeStack.tsx
@@ -1,46 +1,15 @@
-import React, {useEffect, type PropsWithChildren} from 'react';
-import {
-  Button,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import {NavigationContainer, RouteProp} from '@react-navigation/native';
-import {
-  createStackNavigator,
-  StackNavigationEventMap,
-} from '@react-navigation/stack';
-
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 import Workout from './WorkoutScreen';
 import ListOfExercisesScreen from './ListOfExercisesScreen';
-import {HomeTabParamList} from '../types';
-import StatisticsScreen from './StatisticsScreen';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import ExerciseScreen from './ExerciseScreen';
+import { HomeTabParamList } from '../types';
 
 const HomeStack = createStackNavigator<HomeTabParamList>();
 
 const HomeNavigator = () => {
   return (
-    <HomeStack.Navigator
-      screenListeners={{
-        state: (e: any) => {
-          // Do something with the state
-          console.log('state changed HomeNavigator', e.data.state);
-        },
-      }}>
+    <HomeStack.Navigator>
       <HomeStack.Screen
         name="CurrentWorkout"
         component={Workout}
