a0dce0cd643b1b9d93a5462881d86603e6cb0999
Shuliak Lyubov <shuliak.lyubov@gmail.com>
2022-10-29 22:36:48 +0300

    linter

--- App.tsx
+++ App.tsx
@@ -1,100 +1,75 @@
-import React, {
-  useCallback,
-  useEffect,
-  useState,
-  type PropsWithChildren,
-} from 'react';
-import {
-  Button,
-  Image,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React from 'react';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import {
   DarkTheme,
   NavigationContainer,
-  RouteProp,
-  useNavigation,
   useNavigationContainerRef,
-  useRoute,
 } from '@react-navigation/native';
-import {
-  createStackNavigator,
-  StackNavigationProp,
-} from '@react-navigation/stack';
 
-import Workout from './src/screens/WorkoutScreen';
-import ListOfExercisesScreen from './src/screens/ListOfExercisesScreen';
-import {RootStackParamList} from './src/types';
+import { RootStackParamList } from './src/types';
 import HomeNavigator from './src/screens/HomeStack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import StatisticsScreen from './src/screens/StatisticsScreen';
 import Profile from './src/screens/Profile';
 import MyTabBar from './src/components/TabBar';
-import ExerciseScreen from './src/screens/ExerciseScreen';
+import { store } from './src/redux/store';
+import { Provider } from 'react-redux';
 
 const TabStack = createBottomTabNavigator<RootStackParamList>();
 
 const App = () => {
   const navigationRef = useNavigationContainerRef();
   // You can also use a regular ref with `React.useRef()`
-  const myTheme = {DarkTheme};
+
   return (
-    <NavigationContainer
-      ref={navigationRef}
-      theme={{
-        dark: false,
-        colors: {...DarkTheme.colors, background: '#36404F'},
-      }}>
-      <TabStack.Navigator
-        initialRouteName="Home"
-        screenOptions={{tabBarStyle: {backgroundColor: '#343D54'}}}
-        tabBar={props => <MyTabBar {...props} navigationRef={navigationRef} />}>
-        <TabStack.Screen
-          name="Home"
-          component={HomeNavigator}
-          options={{
-            title: 'My workout',
-            headerShown: false,
-            tabBarActiveTintColor: 'white',
-            tabBarInactiveTintColor: 'transparent',
+    <Provider store={store}>
+      <NavigationContainer
+        ref={navigationRef}
+        theme={{
+          dark: false,
+          colors: { ...DarkTheme.colors, background: '#36404F' },
+        }}>
+        <TabStack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            tabBarStyle: { backgroundColor: '#343D54' },
           }}
-        />
-        <TabStack.Screen
-          name="Statistics"
-          component={StatisticsScreen}
-          options={{
-            title: 'Statistics',
-            headerShown: false,
-            tabBarActiveTintColor: 'white',
-            tabBarInactiveTintColor: 'transparent',
-          }}
-        />
-        <TabStack.Screen
-          name="Profile"
-          component={Profile}
-          options={{
-            title: 'Profile',
-            headerShown: false,
-            tabBarActiveTintColor: 'white',
-            tabBarInactiveTintColor: 'transparent',
-          }}
-        />
-      </TabStack.Navigator>
-    </NavigationContainer>
+          tabBar={props => (
+            <MyTabBar {...props} navigationRef={navigationRef} />
+          )}>
+          <TabStack.Screen
+            name="Home"
+            component={HomeNavigator}
+            options={{
+              title: 'My workout',
+              headerShown: false,
+              tabBarActiveTintColor: 'white',
+              tabBarInactiveTintColor: 'transparent',
+            }}
+          />
+          <TabStack.Screen
+            name="Statistics"
+            component={StatisticsScreen}
+            options={{
+              title: 'Statistics',
+              headerShown: false,
+              tabBarActiveTintColor: 'white',
+              tabBarInactiveTintColor: 'transparent',
+            }}
+          />
+          <TabStack.Screen
+            name="Profile"
+            component={Profile}
+            options={{
+              title: 'Profile',
+              headerShown: false,
+              tabBarActiveTintColor: 'white',
+              tabBarInactiveTintColor: 'transparent',
+            }}
+          />
+        </TabStack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
--- src/screens/ListOfExercisesScreen.tsx
+++ src/screens/ListOfExercisesScreen.tsx
@@ -1,34 +1,20 @@
-import {
-  Link,
-  RouteProp,
-  useNavigation,
-  useRoute,
-} from '@react-navigation/native';
-import {StackNavigationProp} from '@react-navigation/stack';
-import React, {useEffect} from 'react';
-import {Button, SafeAreaView, Text} from 'react-native';
-import {
-  HomeTabParamList,
-  HomeTabScreenProps,
-  ListOfExercise,
-  ListOfExerciseRoute,
-  RootStackParamList,
-  RootStackScreenProps,
-} from '../types';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import React from 'react';
+import { Button, SafeAreaView, Text } from 'react-native';
+import { HomeTabParamList, ListOfExerciseRoute } from '../types';
 
 const ListOfExercisesScreen = () => {
   const navigation = useNavigation<StackNavigationProp<HomeTabParamList>>();
   const route = useRoute<ListOfExerciseRoute>();
-
   return (
     <SafeAreaView>
-      <Text>List of exercises {route.params.caption}</Text>
+      <Text>List of exercises {route.params.caption} </Text>
       <Button
         title="Back"
-        onPress={() => navigation.push('CurrentWorkout', {caption: 'feef'})}
+        onPress={() => navigation.push('CurrentWorkout', { caption: 'feef' })}
       />
     </SafeAreaView>
   );
 };
-
 export default ListOfExercisesScreen;
