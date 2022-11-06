7f2006a355af2843872b6324480db7cd74a6c415
Shuliak Lyubov <shuliak.lyubov@gmail.com>
2022-11-06 20:26:00 +0200

    Rename

--- App.tsx
+++ App.tsx
@@ -4,53 +4,28 @@ import {
   NavigationContainer,
   useNavigationContainerRef,
 } from '@react-navigation/native';
-
 import { RootStackParamList } from './src/types/types';
 import { CustomTheme } from './src/theme';
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
 import BottomStackNavigator from './src/stackNavigators/BotomStack';
-import { LoadingGifs } from './src/components/modals/LoadingGifs';
-import { View } from 'react-native';
+import { LoadingExercises } from './src/components/modals/LoadingExercises';
+import { Text, View } from 'react-native';
 
 const App = () => {
   const navigationRef = useNavigationContainerRef<RootStackParamList>();
 
-  // const [initializing, setInitializing] = useState(true);
-  // const [user, setUser] = useState();
-  // const onAuthStateChanged = useCallback(
-  //   (userDat: any) => {
-  //     setUser(userDat);
-  //     if (initializing) {
-  //       setInitializing(false);
-  //     }
-  //   },
-  //   [initializing]
-  // );
-
-  // const signIn = async () => {
-  //   try {
-  //     const userSub = async () => await firebase.auth().signInAnonymously();
-  //     return userSub;
-  //   } catch (error) {}
-  // };
-
-  // useEffect(() => {
-  //   const userDAta = firebase.auth().onAuthStateChanged(onAuthStateChanged);
-
-  //   return userDAta;
-  // }, []);
-
   return (
     <Provider store={store}>
       <View style={{ flex: 1, backgroundColor: CustomTheme.colors.background }}>
-        <LoadingGifs />
+        <LoadingExercises />
         <NavigationContainer
           ref={navigationRef}
           theme={{
             dark: false,
-            colors: { ...CustomTheme.colors },
+            colors: { ...CustomTheme.colors, text: '#FFFFFF' },
           }}
+          fallback={<Text>Loading..</Text>}
         >
           <BottomStackNavigator navigationRef={navigationRef} />
         </NavigationContainer>
