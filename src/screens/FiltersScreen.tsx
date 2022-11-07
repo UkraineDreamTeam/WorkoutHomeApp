f1873a790351de82a649106769b2d5f507855f94
Shuliak Lyubov <shuliak.lyubov@gmail.com>
2022-11-07 23:38:40 +0200

    filters ui

--- src/screens/FiltersScreen.tsx
+++ src/screens/FiltersScreen.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Dimensions } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Dimensions, SafeAreaView, View, Text } from 'react-native';
 import FiltersSection from '../components/filters/FiltersSection.component';
+import BackIcon from '../components/icons/BackIcon.component';
 import { filters } from '../redux/exercises/exercises.slice';
 import { useAppSelector } from '../redux/store';
+import { COLORS } from '../theme';
 
 const FiltersScreen = () => {
   const bodyParts = useAppSelector(filters.bodyPartsList);
@@ -12,6 +13,39 @@ const FiltersScreen = () => {
   const targets = useAppSelector(filters.targetsList);
   return (
     <SafeAreaView style={[{ width: Dimensions.get('screen').width }]}>
+      <View
+        style={[
+          {
+            height: 40,
+            alignSelf: 'flex-start',
+            padding: 10,
+            paddingTop: 15,
+            flexDirection: 'row',
+            justifyContent: 'flex-start',
+            display: 'flex',
+            alignContent: 'flex-start',
+            width: Dimensions.get('screen').width,
+          },
+        ]}
+      >
+        <BackIcon />
+        <Text
+          style={[
+            {
+              height: 30,
+              color: COLORS.WHITE,
+              fontSize: 20,
+              alignSelf: 'center',
+              flexGrow: 1,
+              textAlign: 'center',
+              paddingRight: 50,
+            },
+          ]}
+        >
+          Filters
+        </Text>
+      </View>
+
       <FiltersSection key={'Type'} title="Type" list={types} />
       <FiltersSection key={'Muscle'} title="Muscle" list={targets} />
       <FiltersSection key={'Bodyparts'} title="Bodyparts" list={bodyParts} />
--- src/screens/ListOfExercisesScreen.tsx
+++ src/screens/ListOfExercisesScreen.tsx
@@ -1,23 +1,15 @@
-import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native';
 import Loader from '../components/ActivityIndicator.component';
 import ExerciseList from '../components/exercises/ExerciseList.component';
 import { exercises } from '../redux/exercises/exercises.slice';
 import { useAppSelector } from '../redux/store';
-import { RootStackParamList } from '../types/types';
 
 const ListOfExercisesScreen = () => {
-  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
   const exerciseList = useAppSelector(exercises);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <TouchableOpacity onPress={() => navigation.navigate('Filters')}>
-        <Text>Filters</Text>
-      </TouchableOpacity>
       {exerciseList.length ? <ExerciseList /> : <Loader />}
     </SafeAreaView>
   );
