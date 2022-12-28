1dfcdb25b2f10647b26cc996698d7d451c528e84
Shuliak Lyubov <shuliak.lyubov@gmail.com>
2022-12-28 18:46:41 +0100

    form update

---
+++ src/constants/options.ts
@@ -0,0 +1,12 @@
+export const DUMBBELL_RANGE = Array.from(Array(40).keys());
+export const DUMBBELL_RANGE_GR = ['00', '25', '5'];
+export const MINUTES = Array.from(Array(61).keys()).map(el =>
+  `00${el}`.slice(-2)
+);
+export const SECONDS = Array.from(Array(60).keys()).map(el =>
+  `00${el}`.slice(-2)
+);
+
+export const HOURS = Array.from(Array(12).keys()).map(el =>
+  `00${el}`.slice(-2)
+);
