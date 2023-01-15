43374da7d3156573fd0eac60b3f4cbd74e65c014
Shuliak Lyubov <shuliak.lyubov@gmail.com>
2023-01-15 12:02:08 +0100

    added redux workout form

--- src/components/exerciseScreen/exerciseSetForm/selectors/styles.ts
+++ src/components/exerciseScreen/exerciseSetForm/selectors/styles.ts
@@ -1,1 +1,21 @@
-import { StyleSheet } from 'react-native';import { TYPOGRAPHY } from '@shared/theme';export const styles = StyleSheet.create({  container: {    backgroundColor: 'black',    borderRadius: TYPOGRAPHY.BORDER_RADIUS.small,  },  contentContainer: { width: 50 },  searchContainerStyle: {    borderColor: 'white',    width: 0,    backgroundColor: 'transparent',    borderRadius: TYPOGRAPHY.BORDER_RADIUS.average,  },  searchTextInputStyle: {    color: 'white',    borderColor: 'white',  },  modalTitleStyle: {    fontWeight: 'bold',    backgroundColor: 'transparent',    borderRadius: TYPOGRAPHY.BORDER_RADIUS.average,    height: 0,    padding: 0,    margin: 0,    borderColor: 'none',  },  labelStyle: {    color: 'white',    borderRadius: TYPOGRAPHY.BORDER_RADIUS.small,    textAlign: 'center',    padding: 0,  },  textStyle: {    fontSize: 15,    color: 'white',  },  optionContainerStyle: {    backgroundColor: 'black',    width: 50,    justifyContent: 'center',  },  optionTextStyle: {    backgroundColor: 'black',    width: 50,    textAlign: 'center',  },});
+import { StyleSheet } from 'react-native';
+import { COLORS, TYPOGRAPHY } from '@shared/theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textInput: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'black',
+    borderRadius: TYPOGRAPHY.BORDER_RADIUS.small,
+    textAlign: 'center',
+  },
+  text: {
+    padding: 4,
+    color: COLORS.WHITE,
+    fontSize: 15,
+  },
+});
